9d9084796376bd1bddfe90b5ac316f3f79be3a90
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 05:26:37 -0600

    chore: add npm dependency 'daisyui'

---
+++ assets/tailwind.config.cjs
@@ -0,0 +1,112 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
+  daisyui: {
+    logs: false,
+    themes: [
+      {
+        light: {
+          primary: "#0255D2",
+          secondary: "#51575E",
+          accent: "#0255D2",
+          neutral: "#325993",
+          "base-100": "#F6FAFF",
+          info: "#97C0FE",
+          success: "#136941",
+          warning: "#FFC107",
+          error: "#B82030",
+        },
+        dark: {
+          primary: "#0255D2",
+          secondary: "#51575E",
+          accent: "#0255D2",
+          neutral: "#325993",
+          "base-100": "#14243c",
+          info: "#97C0FE",
+          success: "#136941",
+          warning: "#FFC107",
+          error: "#B82030",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {
+      colors: {
+        brand: "#FD4F00",
+      },
+    },
+    // listStyleType: {
+    //   dash: "- ",
+    // },
+  },
+  plugins: [
+    // require("@tailwindcss/forms"),
+    plugin(({ addVariant }) =>
+      addVariant("phx-no-feedback", [".phx-no-feedback&", ".phx-no-feedback &"])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-click-loading", [
+        ".phx-click-loading&",
+        ".phx-click-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-submit-loading", [
+        ".phx-submit-loading&",
+        ".phx-submit-loading &",
+      ])
+    ),
+    plugin(({ addVariant }) =>
+      addVariant("phx-change-loading", [
+        ".phx-change-loading&",
+        ".phx-change-loading &",
+      ])
+    ),
+    // heroicons
+    plugin(function ({ matchComponents, theme }) {
+      let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized");
+      let values = {};
+      let icons = [
+        ["", "/24/outline"],
+        ["-solid", "/24/solid"],
+        ["-mini", "/20/solid"],
+      ];
+      icons.forEach(([suffix, dir]) => {
+        fs.readdirSync(path.join(iconsDir, dir)).map((file) => {
+          let name = path.basename(file, ".svg") + suffix;
+          values[name] = { name, fullPath: path.join(iconsDir, dir, file) };
+        });
+      });
+      matchComponents(
+        {
+          hero: ({ name, fullPath }) => {
+            let content = fs
+              .readFileSync(fullPath)
+              .toString()
+              .replace(/\r?\n|\r/g, "");
+            return {
+              [`--hero-${name}`]: `url('data:image/svg+xml;utf8,${content}')`,
+              "-webkit-mask": `var(--hero-${name})`,
+              mask: `var(--hero-${name})`,
+              "mask-repeat": "no-repeat",
+              "background-color": "currentColor",
+              "vertical-align": "middle",
+              display: "inline-block",
+              width: theme("spacing.5"),
+              height: theme("spacing.5"),
+            };
+          },
+        },
+        { values }
+      );
+    }),
+    require("daisyui"),
+  ],
+};
