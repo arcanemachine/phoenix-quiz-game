77c0b8b7b4367d1e9ab1f58f8279d1635c75de3b
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 14:13:32 -0600

    ops: expose dev server on all interfaces (0.0.0.0)

--- config/dev.exs
+++ config/dev.exs
@@ -12,7 +12,7 @@ config :quiz_game, QuizGame.Repo,
 
 # disable caching; enable debugging and code reloading; configure watchers (e.g. bundlers)
 config :quiz_game, QuizGameWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
