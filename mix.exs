67d92dcd8af25825d8c5325a980c78a3631b1927
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 15:50:02 -0600

    chore: add Hex dependency 'credo'

--- mix.exs
+++ mix.exs
@@ -49,7 +49,8 @@ defmodule QuizGame.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
