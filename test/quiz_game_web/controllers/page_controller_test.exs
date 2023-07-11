baf5a97497d2ceac6cd6df6f539d857712317b33
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 05:48:28 -0600

    feat: add basic homepage layout

--- test/quiz_game_web/controllers/page_controller_test.exs
+++ test/quiz_game_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule QuizGameWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Hello world!"
   end
 end
