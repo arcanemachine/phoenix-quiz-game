8a9f74ba5e0496065b0b359feb701736d37e4136
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 13:45:57 -0600

    chore: remove boilerplate comments and alphabetize sections in 'config/config.exs'

--- config/config.exs
+++ config/config.exs
@@ -1,16 +1,9 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :quiz_game,
   ecto_repos: [QuizGame.Repo]
 
-# Configures the endpoint
+# endpoint
 config :quiz_game, QuizGameWeb.Endpoint,
   url: [host: "localhost"],
   render_errors: [
@@ -20,16 +13,7 @@ config :quiz_game, QuizGameWeb.Endpoint,
   pubsub_server: QuizGame.PubSub,
   live_view: [signing_salt: "L/HSIeMt"]
 
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :quiz_game, QuizGame.Mailer, adapter: Swoosh.Adapters.Local
-
-# Configure esbuild (the version is required)
+# esbuild
 config :esbuild,
   version: "0.17.11",
   default: [
@@ -39,7 +23,18 @@ config :esbuild,
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
 
-# Configure tailwind (the version is required)
+# json
+config :phoenix, :json_library, Jason
+
+# logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# mailer
+config :quiz_game, QuizGame.Mailer, adapter: Swoosh.Adapters.Local
+
+# tailwind
 config :tailwind,
   version: "3.2.7",
   default: [
@@ -51,14 +46,5 @@ config :tailwind,
     cd: Path.expand("../assets", __DIR__)
   ]
 
-# Configures Elixir's Logger
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
-
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
+# environment (must be at the bottom so that overrides work properly)
 import_config "#{config_env()}.exs"
