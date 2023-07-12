9b82112bca752760d43ffc7e97f14d8cd4c27d78
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 21:49:07 -0600

    chore: add Hex dependency 'dialyxir'

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule QuizGame.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
   end
 
