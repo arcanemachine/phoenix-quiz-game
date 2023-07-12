3d7fa02806ce0b25a7e3dbfa2626bdfb8562d7da
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-11 21:39:00 -0600

    chore: add npm dependency 'alpinejs' and allow 'x-' prefix in components

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,51 @@
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
+import "phoenix_html";
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
+// initialize alpinejs
+import Alpine from "alpinejs";
+Alpine.start();
+window.Alpine = Alpine;
 
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
+// initialize livesocket
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  dom: {
+    onBeforeElUpdated(from, to) {
+      const liveSocketInitializeAlpine = (from, to) => {
+        if (!Alpine || !from || !to) return;
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+        for (let index = 0; index < to.children.length; index++) {
+          const from2 = from.children[index];
+          const to2 = to.children[index];
 
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+          if (from2 instanceof HTMLElement && to2 instanceof HTMLElement) {
+            liveSocketInitializeAlpine.call(from2, to2);
+          }
+        }
+
+        if (from._x_dataStack) Alpine.clone(from, to);
+      };
+
+      liveSocketInitializeAlpine(from, to);
+    },
+  },
+  params: { _csrf_token: csrfToken },
+});
+
+// // add alpine store for livesocket
+// Alpine.store("liveSocket", liveSocket);
+
+// show progress bar on live navigation and form submits
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+// expose livesocket on window for debugging
+window.liveSocket = liveSocket;
--- lib/quiz_game_web.ex
+++ lib/quiz_game_web.ex
@@ -68,7 +68,7 @@ defmodule QuizGameWeb do
 
   def html do
     quote do
-      use Phoenix.Component
+      use Phoenix.Component, global_prefixes: ~w(x-)
 
       # Import convenience functions from controllers
       import Phoenix.Controller,
