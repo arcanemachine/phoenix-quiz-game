cca0952758f68ba2f85addb791271c74d90bf80a
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-13 05:42:07 -0600

    ops: specify port and IP address in runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -31,6 +31,9 @@ if config_env() == :prod do
 
   config :quiz_game, QuizGameWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [ip: {0, 0, 0, 0, 0, 0, 0, 0}, port: port],
+    http: [
+      ip: {0, 0, 0, 0},
+      port: port
+    ],
     secret_key_base: secret_key_base
 end
