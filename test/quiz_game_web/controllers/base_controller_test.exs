796ecc8d53bcb7b76d860c2c9687e8695ab6ba9b
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-18 17:51:54 -0600

    refactor: merge redundant base controller tests into single file and move to own directory

--- test/quiz_game_web/controllers/base_controller_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule QuizGameWeb.PageControllerTest do
-  @moduledoc false
-  use QuizGameWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Hello world!"
-  end
-end
