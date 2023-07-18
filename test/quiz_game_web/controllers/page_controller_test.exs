098558ea66f89a078778e390cbc14645e27450ca
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-18 05:16:41 -0600

    test: add smoke tests for '/privacy-policy' and '/terms-of-use'

--- test/quiz_game_web/controllers/page_controller_test.exs
+++ test/quiz_game_web/controllers/page_controller_test.exs
@@ -6,4 +6,14 @@ defmodule QuizGameWeb.BaseControllerTest do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Hello world!"
   end
+
+  test "GET /terms-of-use", %{conn: conn} do
+    conn = get(conn, ~p"/terms-of-use")
+    assert html_response(conn, 200) =~ "Terms of Use"
+  end
+
+  test "GET /privacy-policy", %{conn: conn} do
+    conn = get(conn, ~p"/privacy-policy")
+    assert html_response(conn, 200) =~ "Privacy Policy"
+  end
 end
