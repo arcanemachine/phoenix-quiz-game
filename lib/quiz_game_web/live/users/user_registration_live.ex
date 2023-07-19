b6a767b5b65aafdc11fc89200f331e605efe1e8d
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-19 04:55:04 -0600

    style: Redesign user registration view

--- lib/quiz_game_web/live/users/user_registration_live.ex
+++ lib/quiz_game_web/live/users/user_registration_live.ex
@@ -4,53 +4,28 @@ defmodule QuizGameWeb.UserRegistrationLive do
   alias QuizGame.Users
   alias QuizGame.Users.User
 
-  def render(assigns) do
-    ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Register for an account
-        <:subtitle>
-          Already registered?
-          <.link navigate={~p"/users/log_in"} class="font-semibold text-brand hover:underline">
-            Sign in
-          </.link>
-          to your account now.
-        </:subtitle>
-      </.header>
-
-      <.simple_form
-        for={@form}
-        id="registration_form"
-        phx-submit="save"
-        phx-change="validate"
-        phx-trigger-action={@trigger_submit}
-        action={~p"/users/log_in?_action=registered"}
-        method="post"
-      >
-        <.error :if={@check_errors}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
-
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
-
-        <:actions>
-          <.button phx-disable-with="Creating account..." class="w-full">Create an account</.button>
-        </:actions>
-      </.simple_form>
-    </div>
-    """
-  end
+  @page_title "Register New Account"
 
   def mount(_params, _session, socket) do
     changeset = Users.change_user_registration(%User{})
 
     socket =
       socket
+      |> assign(page_title: @page_title)
       |> assign(trigger_submit: false, check_errors: false)
       |> assign_form(changeset)
 
     {:ok, socket, temporary_assigns: [form: nil]}
+  end
+
+  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
+    form = to_form(changeset, as: "user")
+
+    if changeset.valid? do
+      assign(socket, form: form, check_errors: false)
+    else
+      assign(socket, form: form)
+    end
   end
 
   def handle_event("save", %{"user" => user_params}, socket) do
@@ -75,13 +50,34 @@ defmodule QuizGameWeb.UserRegistrationLive do
     {:noreply, assign_form(socket, Map.put(changeset, :action, :validate))}
   end
 
-  defp assign_form(socket, %Ecto.Changeset{} = changeset) do
-    form = to_form(changeset, as: "user")
+  def render(assigns) do
+    ~H"""
+    <section class="mx-auto">
+      <p class="text-center">To register a new account, enter your account details below.</p>
 
-    if changeset.valid? do
-      assign(socket, form: form, check_errors: false)
-    else
-      assign(socket, form: form)
-    end
+      <.simple_form
+        for={@form}
+        has_errors={@check_errors}
+        id="registration_form"
+        action={~p"/users/log_in?_action=registered"}
+        method="post"
+        phx-change="validate"
+        phx-submit="save"
+        phx-trigger-action={@trigger_submit}
+      >
+        <.input field={@form[:email]} type="email" label="Email" required />
+        <.input field={@form[:password]} type="password" label="Password" required />
+
+        <:actions>
+          <.form_button_cancel />
+          <.form_button_submit />
+        </:actions>
+      </.simple_form>
+
+      <.action_links items={[
+        %{content: "Login to an existing account", navigate: ~p"/users/log_in"}
+      ]} />
+    </section>
+    """
   end
 end
