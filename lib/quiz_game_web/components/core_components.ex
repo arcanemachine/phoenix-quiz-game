a58a3e9d6c8870d31d10180d4b55b894bbafd99c
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-19 16:49:34 -0600

    style: prevent toast message container from overlapping other visible elements

--- lib/quiz_game_web/components/core_components.ex
+++ lib/quiz_game_web/components/core_components.ex
@@ -182,7 +182,7 @@ defmodule QuizGameWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "w-80 sm:w-[25rem] mx-auto p-3 z-50 rounded-lg ring-1",
+        "w-80 sm:w-[25rem] p-3 z-50 rounded-lg ring-1",
         @kind == :info &&
           "bg-info text-info-content ring-info-content fill-info-content shadow-xl",
         @kind == :success &&
@@ -224,7 +224,8 @@ defmodule QuizGameWeb.CoreComponents do
 
   def flash_group(assigns) do
     ~H"""
-    <div class="fixed w-screen top-1.5 lg:mt-2 right-0 left-0 mt-child-2">
+    <div class="fixed flex flex-col justify-center w-80 sm:w-[25rem] top-1.5 lg:top:3.5 right-0
+                left-0 mt-child-2 mx-auto">
       <.flash kind={:error} title="Error" id="flash-error" flash={@flash} />
       <.flash kind={:warning} title="Warning" id="flash-warning" flash={@flash} />
       <.flash kind={:success} title="Success" id="flash-success" flash={@flash} />
