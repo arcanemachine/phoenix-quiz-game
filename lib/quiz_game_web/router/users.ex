a54a7b55e8dd24d3fa4fa72ab5e9f999691565c5
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-23 15:02:41 -0600

    refactor: remove redundancy in router scopes

--- lib/quiz_game_web/router/users.ex
+++ lib/quiz_game_web/router/users.ex
@@ -6,48 +6,48 @@ defmodule QuizGameWeb.Router.Users do
   def users_browser do
     quote do
       # allow any user
-      scope "/", QuizGameWeb do
+      scope "/users", QuizGameWeb do
         pipe_through(:browser)
 
-        get "/users/logout", UserSessionController, :logout_confirm
-        post "/users/logout", UserSessionController, :logout
+        get "/logout", UserSessionController, :logout_confirm
+        post "/logout", UserSessionController, :logout
 
         live_session :current_user,
           on_mount: [{QuizGameWeb.UserAuth, :mount_current_user}] do
-          live "/users/confirm/email", UserConfirmationInstructionsLive, :new
-          live "/users/confirm/email/:token", UserConfirmationLive, :edit
+          live "/confirm/email", UserConfirmationInstructionsLive, :new
+          live "/confirm/email/:token", UserConfirmationLive, :edit
         end
       end
 
       # logout required
-      scope "/", QuizGameWeb do
+      scope "/users", QuizGameWeb do
         pipe_through([:browser, :redirect_if_user_is_authenticated])
 
-        post "/users/login", UserSessionController, :create
+        post "/login", UserSessionController, :create
 
         live_session :redirect_if_user_is_authenticated,
           on_mount: [{QuizGameWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-          live "/users/register", UserRegistrationLive, :new
-          live "/users/login", UserLoginLive, :new
-          live "/users/reset-password", UserForgotPasswordLive, :new
-          live "/users/reset-password/:token", UserResetPasswordLive, :edit
+          live "/register", UserRegistrationLive, :new
+          live "/login", UserLoginLive, :new
+          live "/reset-password", UserForgotPasswordLive, :new
+          live "/reset-password/:token", UserResetPasswordLive, :edit
         end
       end
 
       # login required
-      scope "/", QuizGameWeb do
+      scope "/users", QuizGameWeb do
         pipe_through([:browser, :require_authenticated_user])
 
-        get "/users/me", UserSessionController, :show
-        get "/users/me/update", UserSessionController, :settings
-        get "/users/me/delete", UserController, :delete_confirm
-        post "/users/me/delete", UserController, :delete
+        get "/me", UserSessionController, :show
+        get "/me/update", UserSessionController, :settings
+        get "/me/delete", UserController, :delete_confirm
+        post "/me/delete", UserController, :delete
 
         live_session :require_authenticated_user,
           on_mount: [{QuizGameWeb.UserAuth, :ensure_authenticated}] do
-          live "/users/me/update/email", UserUpdateEmailLive, :edit
-          live "/users/me/update/email/confirm/:token", UserUpdateEmailLive, :confirm_email
-          live "/users/me/update/password", UserUpdatePasswordLive, :edit
+          live "/me/update/email", UserUpdateEmailLive, :edit
+          live "/me/update/email/confirm/:token", UserUpdateEmailLive, :confirm_email
+          live "/me/update/password", UserUpdatePasswordLive, :edit
         end
       end
     end
