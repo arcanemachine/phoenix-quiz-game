e7e9cefd74161377317bfcf2265cb86359ee0f7a
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-23 16:00:09 -0600

    refactor: customize email templates

--- lib/quiz_game/users/user_notifier.ex
+++ lib/quiz_game/users/user_notifier.ex
@@ -25,19 +25,14 @@ defmodule QuizGame.Users.UserNotifier do
   Deliver instructions to confirm account.
   """
   def deliver_confirmation_instructions(user, url) do
-    deliver(user.email, "Confirmation instructions", """
-
-    ==============================
-
-    Hi #{user.email},
+    deliver(user.email, "Welcome to #{Application.get_env(:quiz_game, :project_name)}!", """
+    Welcome to #{Application.get_env(:quiz_game, :project_name)}!
 
     You can confirm your account by visiting the URL below:
 
     #{url}
 
     If you didn't create an account with us, please ignore this.
-
-    ==============================
     """)
   end
 
@@ -45,39 +40,41 @@ defmodule QuizGame.Users.UserNotifier do
   Deliver instructions to reset a user password.
   """
   def deliver_reset_password_instructions(user, url) do
-    deliver(user.email, "Reset password instructions", """
+    deliver(
+      user.email,
+      "#{Application.get_env(:quiz_game, :project_name)} - Reset Your Password",
+      """
+      You can reset your password by visiting the URL below:
 
-    ==============================
+      #{url}
 
-    Hi #{user.email},
+      If you didn't request this change, please ignore this email.
 
-    You can reset your password by visiting the URL below:
+      ---
 
-    #{url}
-
-    If you didn't request this change, please ignore this.
-
-    ==============================
-    """)
+      Thanks for using #{Application.get_env(:quiz_game, :project_name)}!
+      """
+    )
   end
 
   @doc """
   Deliver instructions to update a user email.
   """
   def deliver_update_email_instructions(user, url) do
-    deliver(user.email, "Update email instructions", """
+    deliver(
+      user.email,
+      "#{Application.get_env(:quiz_game, :project_name)} - Confirm New Email",
+      """
+      Confirm your new email address by visiting the URL below:
 
-    ==============================
+      #{url}
 
-    Hi #{user.email},
+      If you didn't request this change, please ignore this.
 
-    You can change your email by visiting the URL below:
+      ---
 
-    #{url}
-
-    If you didn't request this change, please ignore this.
-
-    ==============================
-    """)
+      Thanks for using #{Application.get_env(:quiz_game, :project_name)}!
+      """
+    )
   end
 end
