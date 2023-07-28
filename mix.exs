51c0922b0f3c76d949aeb0c6a60e51dd724aa6e8
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-27 18:57:07 -0600

    chore: add Hex dependency 'hcaptcha'

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule QuizGame.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:hcaptcha, git: "https://github.com/arcanemachine/hcaptcha", tag: "v0.0.2"}
     ]
   end
 
