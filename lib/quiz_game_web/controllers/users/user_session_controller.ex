1a0e396df41b4eaaa550041a594f07a0edb1c620
Nicholas Moen <arcanemachine@tutanota.com>
2023-07-31 22:46:52 -0600

    refactor: confirm captcha validity on the backend when logging in a user

--- lib/quiz_game_web/controllers/users/user_session_controller.ex
+++ lib/quiz_game_web/controllers/users/user_session_controller.ex
@@ -1,6 +1,8 @@
 defmodule QuizGameWeb.UserSessionController do
   use QuizGameWeb, :controller
 
+  import QuizGameWeb.Conn, only: [text_response: 3]
+
   alias QuizGame.Users
   alias QuizGameWeb.UserAuth
 
@@ -14,8 +16,13 @@ defmodule QuizGameWeb.UserSessionController do
     |> create(params, "Password updated successfully")
   end
 
-  def create(conn, params) do
-    create(conn, params, "Logged in successfully")
+  def create(conn, form_params) do
+    # validate the captcha before finishing the login process
+    if QuizGameWeb.Support.form_captcha_valid?(form_params) do
+      create(conn, form_params, "Logged in successfully")
+    else
+      text_response(conn, 401, "You must complete the human test at the bottom of the form.")
+    end
   end
 
   defp create(conn, %{"user" => user_params}, success_message) do
