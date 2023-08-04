f784d27292b7e87c00723364da96c74c47557ccc
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-04 02:11:16 -0600

    chore(test): improve test assertion helper functions

--- test/support/assertions.ex
+++ test/support/assertions.ex
@@ -2,23 +2,26 @@ defmodule QuizGameWeb.Assertions do
   @moduledoc "Assertions that are commonly used across tests"
   import Phoenix.ConnTest
 
-  def template_has_title(conn, title) do
+  def response_template_has_title(conn, title) do
     html_response(conn, 200) |> Floki.find("h1") |> Floki.raw_html() =~ title
   end
 
-  def template_contains_element(conn, selector) do
+  def response_template_has_text(conn, text) do
+    html_response(conn, 200) =~ text
+  end
+
+  def response_template_has_element(conn, selector) do
     element = html_response(conn, 200) |> Floki.find(selector)
     !Enum.empty?(element)
   end
 
-  # def element_contains_content(conn, selector, content) do
-  #   html_response(conn, 200) |> Floki.find(selector) |> Floki.raw_html() =~ content
+  # def element_contains_text(conn, selector, text) do
+  #   html_response(conn, 200) |> Floki.find(selector) |> Floki.raw_html() =~ text
   # end
 
   def form_has_errors(conn) do
     html_response(conn, 200)
     |> Floki.find(~s|[data-test-label="alert-form-errors"]|)
-    |> Floki.raw_html() =~
-      "Fix the errors in the form"
+    |> Floki.raw_html() =~ "Fix the errors in the form"
   end
 end
