0cd8f35e9356405536e12f028f9d7dbf616e4b42
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-04 19:17:36 -0600

    feat: add helper function to count the number of objects in a given table

--- lib/quiz_game/repo.ex
+++ lib/quiz_game/repo.ex
@@ -2,4 +2,15 @@ defmodule QuizGame.Repo do
   use Ecto.Repo,
     otp_app: :quiz_game,
     adapter: Ecto.Adapters.Postgres
+
+  import Ecto.Query, warn: false
+
+  @doc """
+  Shows the number of objects in a given table.
+
+  ## Examples
+
+      iex> count("users")
+  """
+  def count(table_name), do: one(from t in table_name, select: count(t.id))
 end
