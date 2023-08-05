47e202ca06a9d666b9bb816a258bfbc21cb82c3a
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-05 05:55:19 -0600

    test: add test for User context

---
+++ test/quiz_game/users/user_test.exs
@@ -0,0 +1,13 @@
+defmodule QuizGame.UserTest do
+  @moduledoc false
+
+  use QuizGame.DataCase
+
+  import QuizGame.Users.User
+
+  describe "password_length_min/0" do
+    test "returns expected value" do
+      assert password_length_min() == 8
+    end
+  end
+end
