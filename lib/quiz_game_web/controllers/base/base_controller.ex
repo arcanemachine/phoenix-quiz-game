03b4e4df68bde03f30fa908feff16cdef4547c33
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-08 16:29:02 -0600

    chore: improve homepage <title>

--- lib/quiz_game_web/controllers/base/base_controller.ex
+++ lib/quiz_game_web/controllers/base/base_controller.ex
@@ -2,7 +2,10 @@ defmodule QuizGameWeb.BaseController do
   use QuizGameWeb, :controller
 
   def root(conn, _params) do
-    render(conn, :root, page_title: Application.get_env(:quiz_game, :project_name))
+    render(conn, :root,
+      tag_title: "Home",
+      page_title: Application.get_env(:quiz_game, :project_name)
+    )
   end
 
   def privacy_policy(conn, _params) do
