8a4af5b7989e38b518564ca15d39bcd84077d6a7
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-09 16:14:31 -0600

    chore: add example quiz via seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,27 +1,28 @@
 # import Ecto.Query
 
-# alias QuizGame.Quizzes
+alias QuizGame.Quizzes
 # alias QuizGame.Quizzes.{Card, Quiz}
 # alias QuizGame.Users.User
 alias QuizGame.Users
 
 if Application.get_env(:quiz_game, :server_environment) == :dev do
   # create admin user
-  {:ok, user} =
+  {:ok, admin_user} =
     Users.register_user(%{
       username: "admin",
       email: "admin@example.com",
       password: "password"
     })
 
-  Users.update_user_is_admin(user, true)
+  Users.update_user_is_admin(admin_user, true)
 
   # create non-admin user
-  Users.register_user(%{
-    username: "user",
-    email: "user@example.com",
-    password: "password"
-  })
+  {:ok, non_admin_user} =
+    Users.register_user(%{
+      username: "user",
+      email: "user@example.com",
+      password: "password"
+    })
 
   # # create other users
   # for i <- 1..5 do
@@ -32,6 +33,12 @@ if Application.get_env(:quiz_game, :server_environment) == :dev do
   #   })
   # end
 
+  # create example quiz
+  Quizzes.create_quiz(%{
+    name: "Example Quiz",
+    user_id: non_admin_user.id
+  })
+
   # # create quizzes for all generated users (except the primary user)
   # user_ids = Repo.all(from u in User, select: u.id)
 
@@ -39,8 +46,8 @@ if Application.get_env(:quiz_game, :server_environment) == :dev do
   #   if user_id != primary_user.id do
   #     for i <- Enum.random(0..3) do
   #       Quizzes.create_quiz(%{
-  #         user_id: user_id,
   #         name: "Quiz ##{user_id}.#{i}"
+  #         user_id: user_id,
   #       })
   #     end
   #   end
