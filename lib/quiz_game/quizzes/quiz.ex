25d62ffe8240fc720032e04bfba9bc58ea6c5e2d
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-25 16:34:56 -0600

    refactor: when disabling math random questions, reset all related random value settings

--- lib/quiz_game/quizzes/quiz.ex
+++ lib/quiz_game/quizzes/quiz.ex
@@ -49,9 +49,12 @@ defmodule QuizGame.Quizzes.Quiz do
     quiz
     |> cast(attrs, @safe_fields_required ++ @safe_fields_optional)
     |> validate_required(@safe_fields_required)
+    |> validate_number(:math_random_question_count,
+      greater_than_or_equal_to: math_random_question_count_min(),
+      less_than_or_equal_to: math_random_question_count_max()
+    )
+    |> validate_subject()
     |> foreign_key_constraint(:user_id)
-    # |> validate_math_quiz()
-    # |> validate_non_math_quiz()
   end
 
   @doc "A changeset that contains one or more fields that should not be modified by the user."
@@ -62,9 +65,66 @@ defmodule QuizGame.Quizzes.Quiz do
     |> cast(attrs, @unsafe_fields_required ++ @safe_fields_required ++ @safe_fields_optional)
     |> validate_required(@unsafe_fields_required ++ @safe_fields_required)
     |> validate_length(:name, max: name_length_max())
+    |> validate_number(:math_random_question_count,
+      greater_than_or_equal_to: math_random_question_count_min(),
+      less_than_or_equal_to: math_random_question_count_max()
+    )
+    |> validate_subject()
     |> foreign_key_constraint(:user_id)
-    # |> validate_math_quiz()
-    # |> validate_non_math_quiz()
+  end
+
+  @spec changeset_field_is_or_will_be(Ecto.Changeset.t(), atom(), any()) :: boolean()
+  def changeset_field_is_or_will_be(changeset, field, value) do
+    if Map.get(changeset.changes, field) == value do
+      # matching value in changes
+      true
+    else
+      # existing value matches and other value not in changes
+      if get_in(changeset.data, [Access.key(field)]) == value && !Map.has_key?(changeset, field) do
+        true
+      else
+        false
+      end
+    end
+  end
+
+  # @spec changeset_get_changed_or_existing_value(Ecto.Changeset.t(), atom(), any()) :: any()
+  # def changeset_get_changed_or_existing_value(changeset, field, default) do
+  #   :fixme
+  # end
+
+  @spec validate_subject(Ecto.Changeset.t()) :: Ecto.Changeset.t()
+  def validate_subject(changeset) do
+    if changeset |> changeset_field_is_or_will_be(:subject, :math) do
+      changeset |> validate_subject_math()
+    else
+      changeset_remove_math_random_question_data(changeset)
+    end
+  end
+
+  @doc "Validations for quizzes whose subject is 'math'."
+  @spec validate_subject_math(Ecto.Changeset.t()) :: Ecto.Changeset.t()
+  def validate_subject_math(changeset) do
+    case Map.get(changeset.changes, :math_random_question_count) do
+      0 ->
+        changeset_remove_math_random_question_data(changeset)
+
+      _ ->
+        changeset
+
+        # |> validate_number(:math_random_question_count_max, greater_than: changeset_get_changed_or_existing_value(changeset, :math_random_question_value_min))
+    end
+  end
+
+  @spec changeset_remove_math_random_question_data(Ecto.Changeset.t()) :: Ecto.Changeset.t()
+  def changeset_remove_math_random_question_data(changeset) do
+    changeset
+    |> change(
+      math_random_question_count: 0,
+      math_random_question_operations: nil,
+      math_random_question_value_min: nil,
+      math_random_question_value_max: nil
+    )
   end
 
   # @doc "Returns a changeset with all unsafe parameters removed."
