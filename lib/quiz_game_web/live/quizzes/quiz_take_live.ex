cca8d3fc8e1f7581c15b4ea795c2d93d600d4f73
Nicholas Moen <arcanemachine@tutanota.com>
2023-08-30 02:39:29 -0600

    refactor: improve quiz game logic

--- lib/quiz_game_web/live/quizzes/quiz_take_live.ex
+++ lib/quiz_game_web/live/quizzes/quiz_take_live.ex
@@ -8,13 +8,15 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
 
   @impl Phoenix.LiveView
   def mount(params, _session, socket) do
-    quiz = _quiz_get_or_404(params)
+    # get quiz or 404
+    query = from q in Quiz, where: q.id == ^params["quiz_id"], preload: [:cards]
+    quiz = Support.Repo.record_get_or_404(query)
 
     # redirect if quiz does not have any cards or random math questions
     if Enum.empty?(quiz.cards) && !quiz.math_random_question_count do
       {:ok,
        socket
-       |> put_flash(:error, "This quiz cannot be taken because it has no questions.")
+       |> put_flash(:error, "This quiz cannot be taken because it has no cards.")
        |> redirect(to: route(:quizzes, :show, quiz_id: quiz.id))}
     else
       {:ok,
@@ -27,7 +29,29 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
     end
   end
 
+  defp _socket_initialize(socket) do
+    user = socket.assigns.current_user
+
+    socket
+    |> assign(
+      quiz: socket.assigns.quiz,
+      quiz_state: (user && :before_start) || :enter_display_name,
+      display_name: (user && user.display_name) || nil,
+      current_card_index: 0,
+      card: socket.assigns.quiz.cards |> Enum.at(0),
+      score: 0
+    )
+  end
+
   @impl Phoenix.LiveView
+  def handle_event("submit-display-name", %{"display-name" => display_name}, socket) do
+    if String.trim(display_name) != "" do
+      {:noreply, socket |> assign(display_name: display_name, quiz_state: :before_start)}
+    else
+      {:noreply, socket |> put_flash(:error, "You must enter a display name.")}
+    end
+  end
+
   # def handle_event("change-display-name", _params, socket) do
   #   socket = socket |> clear_flash()
 
@@ -48,17 +72,9 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
      |> assign(quiz_state: :in_progress)}
   end
 
-  def handle_event("submit-display-name", %{"display-name" => display_name}, socket) do
-    if String.trim(display_name) != "" do
-      {:noreply, socket |> assign(display_name: display_name, quiz_state: :before_start)}
-    else
-      {:noreply, socket |> put_flash(:error, "You must enter a display name.")}
-    end
-  end
-
   def handle_event("submit-user-answer", params, %{assigns: assigns} = socket) do
-    user_answer = _answer_user_get(assigns.card, params)
-    correct_answer = _answer_correct_get(assigns.card)
+    user_answer = _get_user_answer(assigns.card, params)
+    correct_answer = _get_correct_answer(assigns.card)
 
     # check if answer was correct and dispatch the appropriate actions
     socket =
@@ -77,19 +93,13 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
       end
 
     # check if quiz is completed
-    if assigns.current_card_index + 1 == _quiz_length_get(assigns.quiz) do
-      # quiz is completed. update quiz_state
+    quiz_is_completed = assigns.current_card_index == _get_quiz_length(assigns.quiz) - 1
+
+    if quiz_is_completed do
       {:noreply, socket |> assign(quiz_state: :completed)}
     else
-      # quiz is still in progress. get next card and increment current card index
-      next_card = _card_next_get(assigns.quiz, assigns.current_card_index)
-
-      {:noreply,
-       socket
-       |> assign(
-         card: next_card,
-         current_card_index: assigns.current_card_index + 1
-       )}
+      # get next card and increment current card index
+      {:noreply, socket |> _assign_next_card_and_index()}
     end
   end
 
@@ -97,8 +107,9 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
     {:noreply, _socket_initialize(socket)}
   end
 
-  @spec _answer_correct_get(Card) :: [integer() | String.t()]
-  defp _answer_correct_get(card) do
+  # answer
+  @spec _get_correct_answer(Card) :: [integer() | String.t()]
+  defp _get_correct_answer(card) do
     case card.format do
       :multiple_choice ->
         # convert choice index to actual answer
@@ -117,32 +128,30 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
     end
   end
 
-  @spec _answer_user_convert_to_choice_atom(String.t()) ::
-          :choice_1 | :choice_2 | :choice_3 | :choice_4
-  defp _answer_user_convert_to_choice_atom(user_answer) do
-    ## Safely converts `user_answer` param to one of the 4 Card choice atoms.
-    ## If a bad value is detected, the answer is converted to 1.
+  @spec _get_user_answer(Card, map()) :: [integer() | String.t()]
+  defp _get_user_answer(card, params) do
+    get_choice_atom_from_user_answer = fn user_answer ->
+      ## Safely converts `user_answer` param to one of the 4 Card choice atoms.
+      ## If a bad value is detected, the answer is converted to 1.
 
-    # convert choice to integer (fallback to 1 for invalid value)
-    choice_int =
-      try do
-        String.to_integer(user_answer)
-      rescue
-        _ -> 1
-      end
+      # convert choice to integer (fallback to 1 for invalid value)
+      choice_int =
+        try do
+          String.to_integer(user_answer)
+        rescue
+          _ -> 1
+        end
 
-    # ensure value is in the range of 1-4
-    choice_int = if choice_int in 1..4, do: choice_int, else: 1
+      # ensure value is in the range of 1-4
+      choice_int = if choice_int in 1..4, do: choice_int, else: 1
 
-    String.to_existing_atom("choice_#{choice_int}")
-  end
+      String.to_existing_atom("choice_#{choice_int}")
+    end
 
-  @spec _answer_user_get(Card, map()) :: [integer() | String.t()]
-  defp _answer_user_get(card, params) do
     case card.format do
       :multiple_choice ->
         # convert choice index to actual answer
-        user_choice = _answer_user_convert_to_choice_atom(params["user-answer"])
+        user_choice = get_choice_atom_from_user_answer.(params["user-answer"])
         user_answer = Map.get(card, user_choice)
 
         user_answer
@@ -151,79 +160,63 @@ defmodule QuizGameWeb.Quizzes.QuizTakeLive do
         String.to_integer(params["user-answer"])
 
       :number_entry ->
-        String.to_integer(params["user-answer"])
+        params["user-answer"]
 
       _ ->
         String.downcase(params["user-answer"])
     end
   end
 
-  defp _card_next_get(quiz, current_card_index) do
-    if quiz.math_random_question_count do
-      # build random math question
-      first_value =
-        Enum.random(quiz.math_random_question_value_min..quiz.math_random_question_value_max)
+  # assigns
+  defp _assign_next_card_and_index(socket) do
+    quiz = socket.assigns.quiz
+    next_card_index = socket.assigns.current_card_index + 1
 
-      second_value =
-        Enum.random(quiz.math_random_question_value_min..quiz.math_random_question_value_max)
+    card =
+      if quiz.math_random_question_count do
+        min = quiz.math_random_question_value_min
+        max = quiz.math_random_question_value_max
 
-      operation = Enum.random(quiz.math_random_question_operations)
+        # build random math question
+        first_value = Enum.random(min..max)
+        second_value = Enum.random(min..max)
+        operation = Enum.random(quiz.math_random_question_operations)
 
-      # get string values so we can display the question to the user
-      operation_as_string =
-        case operation do
-          :add -> "+"
-          :subtract -> "-"
-          :multiply -> "×"
-          :divide -> "÷"
-        end
+        # get string values so we can display the question to the user
+        operation_as_string =
+          case operation do
+            :add -> "+"
+            :subtract -> "-"
+            :multiply -> "×"
+            :divide -> "÷"
+          end
 
-      question_as_string = "#{first_value} #{operation_as_string} #{second_value} = ?"
+        question_as_string = "#{first_value} #{operation_as_string} #{second_value} = ?"
 
-      # generate card with random math question data
-      %{
-        format: :random_math_question,
-        question: question_as_string,
-        first_value: first_value,
-        second_value: second_value,
-        operation: operation
-      }
-    else
-      # get next card for the quiz
-      quiz.cards |> Enum.at(current_card_index + 1)
-    end
+        # return card-like map containing random math question data
+        %{
+          format: :random_math_question,
+          question: question_as_string,
+          first_value: first_value,
+          second_value: second_value,
+          operation: operation
+        }
+      else
+        # get next card for the quiz
+        quiz.cards |> Enum.at(next_card_index)
+      end
+
+    socket |> assign(card: card, current_card_index: next_card_index)
   end
 
-  defp _progress_percentage_get_as_integer(assigns) do
-    (assigns.current_card_index / _quiz_length_get(assigns.quiz) * 100) |> round() |> trunc()
-  end
-
-  defp _quiz_get_or_404(params) do
-    query = from q in Quiz, where: q.id == ^params["quiz_id"], preload: [:cards]
-    Support.Repo.record_get_or_404(query)
-  end
-
-  defp _quiz_length_get(quiz) do
+  # quiz
+  defp _get_quiz_length(quiz) do
     # add the number of cards to the number of random math questions
-    length(quiz.cards) + quiz.math_random_question_count
+    length(quiz.cards) + (quiz.math_random_question_count || 0)
   end
 
-  defp _score_percentage_get_as_integer(assigns) do
+  # score
+  defp _get_score_percentage_as_integer(assigns) do
     (assigns.score / (assigns.current_card_index + 1) * 100) |> round() |> trunc()
-  end
-
-  defp _socket_initialize(socket) do
-    user = socket.assigns.current_user
-
-    socket
-    |> assign(
-      quiz: socket.assigns.quiz,
-      quiz_state: (user && :before_start) || :enter_display_name,
-      display_name: (user && user.display_name) || nil,
-      # card: socket.assigns.quiz.cards |> Enum.at(0),
-      card: _card_next_get(socket.assigns.quiz, 0),
-      current_card_index: 0,
-      score: 0
-    )
   end
 end
