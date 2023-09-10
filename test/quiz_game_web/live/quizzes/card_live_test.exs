3aba3e0f080030cf0eb50dc509b82f31c9dc81c8
Nicholas Moen <arcanemachine@tutanota.com>
2023-09-10 15:35:06 -0600

    test: finish fixing broken CardLive tests

--- test/quiz_game_web/live/quizzes/card_live_test.exs
+++ test/quiz_game_web/live/quizzes/card_live_test.exs
@@ -4,26 +4,37 @@ defmodule QuizGameWeb.Quizzes.CardLiveTest do
   use QuizGameWeb.ConnCase
 
   import Phoenix.LiveViewTest
-  import QuizGame.TestSupport.{Assertions, QuizzesFixtures, UsersFixtures}
+  import QuizGame.TestSupport.{QuizzesFixtures, UsersFixtures}
 
   @create_attrs %{
+    question: "some question",
     format: :text_entry,
     # image: "some image",
-    question: "some question",
     correct_answer: "some answer"
   }
   @update_attrs %{
+    question: "updated question",
     format: :text_entry,
     # image: "updated image",
-    question: "updated question",
     correct_answer: "updated answer"
   }
   @invalid_attrs %{
-    format: nil,
+    question: nil,
+    format: nil
     # image: nil,
-    question: nil
-    # correct_answer: nil # choice will not appear
+    # correct_answer: nil # do not pass this value if :format is null
   }
+
+  def get_index_url(quiz_id), do: route(:quizzes_cards, :index, quiz_id: quiz_id)
+
+  def get_new_url(quiz_id), do: route(:quizzes_cards, :new, quiz_id: quiz_id)
+
+  def get_show_url(quiz_id, card_id),
+    do: route(:quizzes_cards, :show, quiz_id: quiz_id, card_id: card_id)
+
+  def get_edit_url(quiz_id, card_id) do
+    route(:quizzes_cards, :edit, quiz_id: quiz_id, card_id: card_id)
+  end
 
   setup do
     user = user_fixture()
@@ -33,155 +44,106 @@ defmodule QuizGameWeb.Quizzes.CardLiveTest do
   end
 
   describe ":index" do
-    def get_test_url(quiz_id), do: route(:quizzes_cards, :index, quiz_id: quiz_id)
-
     test "lists all cards", %{conn: conn, card: card, user: user} do
-      {:ok, _view, html} =
-        conn
-        |> login_user(user)
-        |> live(get_test_url(card.quiz_id))
+      {:ok, _view, html} = conn |> login_user(user) |> live(get_index_url(card.quiz_id))
 
+      # template contains expected content
       assert html =~ "Question List"
       assert html =~ card.question
     end
 
     test "saves new card", %{conn: conn, user: user, quiz: quiz} do
-      {:ok, view, _html} =
-        conn
-        |> login_user(user)
-        |> live(get_test_url(quiz.id))
+      {:ok, view, _html} = conn |> login_user(user) |> live(get_index_url(quiz.id))
 
-      assert view |> element("a", "New Question") |> render_click() =~
-               "New Question"
+      # click event on expected element renders expected content
+      assert view
+             |> element("a", "Create new question")
+             |> render_click() =~ "New Question"
 
-      assert_patch(view, ~p"/quizzes/cards/new")
+      # redirects to expected route via live patch
+      assert_patch(view, get_new_url(quiz.id))
 
+      # attempt to submit form with invalid data
       assert view
              |> form("#card-form", card: @invalid_attrs)
+             # change event renders expected form validation errors
              |> render_change() =~ "is required"
 
+      # assign required field(s) so that conditional field(s) will be rendered
       assert view
-             |> form("#card-form", card: @create_attrs)
-             |> render_submit()
+             |> form("#card-form", card: %{format: @create_attrs[:format]})
+             |> render_change()
 
-      assert_patch(view, ~p"/quizzes/cards")
-      # assert_patch(view, route(:quizzes_cards, :index, quiz_id: quiz.id))
+      # submit form with valid data
+      assert view |> form("#card-form", card: @create_attrs) |> render_submit()
 
+      # redirects to expected route via live patch
+      assert_patch(view, get_index_url(quiz.id))
+
+      # template contains expected content
       html = render(view)
       assert html =~ "Question created successfully"
-      assert html =~ "some image"
-    end
-
-    test "updates card in listing", %{conn: conn, card: card} do
-      {:ok, view, _html} = live(conn, ~p"/quizzes/cards")
-
-      assert view |> element("#cards-#{card.id} a", "Edit") |> render_click() =~
-               "Edit Question"
-
-      assert_patch(view, ~p"/quizzes/cards/#{card}/edit")
-
-      assert view
-             |> form("#card-form", card: @invalid_attrs)
-             |> render_change() =~ "is required"
-
-      assert view
-             |> form("#card-form", card: @update_attrs)
-             |> render_submit()
-
-      assert_patch(view, ~p"/quizzes/cards")
-
-      html = render(view)
-      assert html =~ "Question updated successfully"
-      assert html =~ "some updated image"
+      # assert html =~ "some image"
     end
   end
 
   describe ":show" do
-    def get_test_url(quiz_id, card_id),
-      do: route(:quizzes_cards, :show, quiz_id: quiz_id, card_id: card_id)
+    test "displays expected card", %{conn: conn, user: user, card: card} do
+      {:ok, _view, html} = conn |> login_user(user) |> live(get_show_url(card.quiz_id, card.id))
 
-    test "displays card", %{conn: conn, user: user, card: card} do
-      {:ok, _view, html} =
-        conn
-        |> login_user(user)
-        |> live(get_test_url(card.quiz_id, card.id))
-
+      # template contains expected content
       assert html =~ "Question Info"
       # assert html =~ card.image
     end
 
-    test "deletes card in listing", %{conn: conn, user: user, card: card} do
-      success_url = route(:quizzes_cards, :index, quiz_id: card.quiz_id)
+    test "deletes expected card", %{conn: conn, user: user, card: card} do
+      {:ok, view, _html} =
+        conn |> login_user(user) |> live(get_show_url(card.quiz_id, card.id))
 
+      # delete expected record
+      view |> element("[data-test-id='delete-card']") |> render_click()
+
+      # redirects to expected route
+      view |> assert_redirected(get_index_url(card.quiz_id))
+
+      # template does not contain expected content after redirect
+      {:ok, _view, html_after_redirect} =
+        conn |> login_user(user) |> live(get_index_url(card.quiz_id))
+
+      refute html_after_redirect =~ card.question
+    end
+
+    test "updates expected card", %{conn: conn, user: user, card: card} do
       {:ok, view, _html} =
         conn
         |> login_user(user)
-        |> live(get_test_url(card.quiz_id, card.id))
+        |> live(get_show_url(card.quiz_id, card.id))
 
-      # delete the card
-      view
-      |> element("[data-test-id='delete-card']")
-      |> render_click()
-
-      # view redirects to expected route
-      refute view
-             |> assert_redirected(success_url)
-             # index no longer contains expected value
-             |> render()
-             |> html_has_content(card.question)
-    end
-  end
-
-  describe ":edit" do
-    def get_test_url(quiz_id, card_id) do
-      route(:quizzes_cards, :edit, quiz_id: quiz_id, card_id: card_id)
-    end
-
-    def get_success_url(quiz_id), do: route(:quizzes_cards, :index, quiz_id: quiz_id)
-
-    test "does not update card with invalid data", %{conn: conn, user: user, card: card} do
-      edit_url = route(:quizzes_cards, :edit, quiz_id: card.quiz_id, card_id: card.id)
-
-      {:ok, view, _html} =
-        conn
-        |> login_user(user)
-        |> live(get_test_url(card.quiz_id, card.id))
-
+      # click event on expected element renders expected content
       assert view
              |> element("a", "Edit this question")
-             |> render_click() =~
-               "Edit Question"
+             |> render_click() =~ "Edit Question"
 
-      assert_patch(view, edit_url)
+      # redirects to expected route via live patch
+      assert_patch(view, get_edit_url(card.quiz_id, card.id))
 
+      # attempt to submit form with invalid data
       assert view
              |> form("#card-form", card: @invalid_attrs)
-             |> render_change() =~
-               "is required"
-    end
+             |> render_change() =~ "is required"
 
-    test "updates card in modal", %{conn: conn, user: user, card: card} do
-      edit_url = route(:quizzes_cards, :edit, quiz_id: card.quiz_id, card_id: card.id)
-      success_url = route(:quizzes_cards, :show, quiz_id: card.quiz_id, card_id: card.id)
-
-      {:ok, view, _html} =
-        conn
-        |> login_user(user)
-        |> live(get_test_url(card.quiz_id, card.id))
-
+      # assign required field(s) so that conditional field(s) will be rendered
       assert view
-             |> element("a", "Edit this question")
-             |> render_click() =~
-               "Edit Question"
+             |> form("#card-form", card: %{format: @update_attrs[:format]})
+             |> render_change()
 
-      assert_patch(view, edit_url)
+      # submit form with valid data
+      assert view |> form("#card-form", card: @update_attrs) |> render_submit()
 
-      assert view
-             |> form("#card-form", card: @update_attrs)
-             |> render_submit()
+      # redirects to expected route via live patch
+      assert_patch(view, get_show_url(card.quiz_id, card.id))
 
-      assert_patch(view, success_url)
-
+      # template contains expected content
       html = render(view)
       assert html =~ "Question updated successfully"
       # assert html =~ "some updated image"
