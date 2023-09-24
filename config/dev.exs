0ba015f0147b65d0e97fccf65c3de1a874e0ecaa
Nicholas Moen <arcanemachine@tutanota.com>
2023-09-24 03:02:41 -0600

    chore: fix live reload after updating project directory structure

--- config/dev.exs
+++ config/dev.exs
@@ -35,7 +35,7 @@ config :quiz_game, QuizGameWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/quiz_game_web/(controllers|live|components)/.*(ex|heex)$"
+      ~r"lib/quiz_game_web/.*/(html/|live/|components).*(ex|heex|js)$"
     ]
   ]
 
