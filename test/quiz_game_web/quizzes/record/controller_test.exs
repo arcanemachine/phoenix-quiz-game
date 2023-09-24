9d437635bc0c0cd60cba2ff441605ae8e9585bae
Nicholas Moen <arcanemachine@tutanota.com>
2023-09-24 02:44:46 -0600

    test: add tests for quizzes record controller

---
+++ test/quiz_game_web/quizzes/record/controller_test.exs
@@ -0,0 +1,64 @@
+defmodule QuizGameWeb.Quizzes.Record.ControllerTest do
+  @moduledoc false
+
+  use QuizGameWeb.ConnCase
+
+  import Ecto.Query
+  import QuizGame.TestSupport.{Assertions, GenericTests}
+  import QuizGame.TestSupport.Fixtures.{Quizzes, Users}
+  import QuizGameWeb.Support.Router
+
+  alias QuizGame.Quizzes.Quiz
+  alias QuizGame.Repo
+
+  defp _get_record_index_url(quiz_id) do
+    route(:quizzes_record, :index, quiz_id: quiz_id)
+  end
+
+  setup %{conn: conn} do
+    # create users
+    user = user_fixture()
+    other_user = user_fixture()
+
+    # create quiz and records
+    quiz = quiz_fixture(user_id: user.id)
+    record_fixture(%{quiz_id: quiz.id})
+    record_fixture(%{quiz_id: quiz.id})
+
+    # fetch quiz from repo so we can preload the records and easily access them
+    quiz = Repo.one!(from(q in Quiz, where: q.id == ^quiz.id, preload: :records))
+
+    %{conn: conn, user: user, other_user: other_user, quiz: quiz}
+  end
+
+  describe "quiz :index" do
+    test "renders expected markup for permissioned user", %{conn: conn, user: user, quiz: quiz} do
+      other_record = record_fixture()
+
+      html =
+        conn |> login_user(user) |> get(_get_record_index_url(quiz.id)) |> Map.get(:resp_body)
+
+      assert html_has_title(html, "Quiz Records")
+      assert html_has_content(html, quiz.name)
+
+      # template contains expected quiz records
+      for record <- quiz.records do
+        assert html_has_content(html, record.display_name)
+      end
+
+      refute html_has_content(html, other_record.display_name)
+    end
+
+    test "redirects unauthenticated user to login route", %{quiz: quiz} do
+      assert build_conn()
+             |> redirects_unauthenticated_user_to_login_route(_get_record_index_url(quiz.id))
+    end
+
+    test "forbids unpermissioned user", %{other_user: other_user, quiz: quiz} do
+      assert build_conn()
+             |> login_user(other_user)
+             |> get(_get_record_index_url(quiz.id))
+             |> Map.get(:status) == 403
+    end
+  end
+end
