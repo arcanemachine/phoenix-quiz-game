41547a975c8ed72fbdd684300745977df9b65964
Nicholas Moen <arcanemachine@tutanota.com>
2023-10-04 21:09:47 -0600

    refactor: move inlined JS to external script
    Inlining was causing issues since the expected file wasn't available in production.
    
    Moving to external script should resolve this issue.

--- lib/quiz_game_web.ex
+++ lib/quiz_game_web.ex
@@ -17,7 +17,7 @@ defmodule QuizGameWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images js favicon.ico robots.txt)
 
   def channel do
     quote do
